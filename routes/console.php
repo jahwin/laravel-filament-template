94c4342a8ff8a592c760915528dc70bcc99d48ae
Abe Jahwin <ajahwin@gmail.com>
2026-08-20 05:16:04 +0200

    working on php compatibility

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,20 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('server {--host=127.0.0.1} {--port=} {--tries=10} {--no-reload}', function () {
+    $options = [
+        '--host' => $this->option('host'),
+        '--tries' => $this->option('tries'),
+    ];
+
+    if ($this->option('port')) {
+        $options['--port'] = $this->option('port');
+    }
+
+    if ($this->option('no-reload')) {
+        $options['--no-reload'] = true;
+    }
+
+    return $this->call('serve', $options);
+})->purpose('Serve the application on the PHP development server');
